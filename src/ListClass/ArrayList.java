dde2a160d4c1c9feacbe65918a5ff5970b88e6f9
asp4303 <asp4303@gmail.com>
2021-11-18 18:14:26 +0900

    ArrayList 멤버변수 및 생성자 추가

--- src/ListClass/ArrayList.java
+++ src/ListClass/ArrayList.java
@@ -2,7 +2,46 @@ package ListClass;
 
 import InterfaceForList.List;
 
+import java.util.Arrays;
+
 public class ArrayList<E> implements List<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final Object[] EMPTY_ARRAY = {};
+
+    private int size;
+    Object[] array;
+
+    public ArrayList(){
+        this.array = EMPTY_ARRAY;
+        this.size = 0;
+    }
+
+    public ArrayList(int capacity){
+        this.array = new Object[capacity];
+        this.size = 0;
+    }
+
+    public void resize(){
+        int array_capacity = array.length;
+
+        // 용적량이 0인 경우
+        if(Arrays.equals(array, EMPTY_ARRAY)){
+            array = new Object[DEFAULT_CAPACITY];
+            return;
+        }
+
+        // 데이터 개수와 용적량이 같은 경우
+        if(size == array_capacity){
+            int new_capacity = array_capacity * 2;
+            array = Arrays.copyOf(array, new_capacity);
+        }
+        // 데이터 개수가 용적량의 반 미만인 경우
+        if(size < (array_capacity / 2)){
+            int new_capacity = array_capacity / 2;
+            array = Arrays.copyOf(array, Math.max(new_capacity, DEFAULT_CAPACITY));
+        }
+    }
+
     @Override
     public boolean add(E value) {
         return false;
