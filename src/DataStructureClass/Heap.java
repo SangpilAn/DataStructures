45d9459594ed104954c2710587930d2637a1211f
asp4303 <asp4303@gmail.com>
2022-01-20 18:24:00 +0900

    Heap 클래스 add, remove 메서드 구현

--- src/DataStructureClass/Heap.java
+++ src/DataStructureClass/Heap.java
@@ -1,6 +1,7 @@
 package DataStructureClass;
 
 import java.util.Comparator;
+import java.util.NoSuchElementException;
 
 public class Heap<E> {
 
@@ -41,5 +42,164 @@ public class Heap<E> {
 
     private int getRightChild(int index){
         return index * 2 + 1;
+    }
+
+    private void resize(int newCapacity){
+
+        Object[] newArray = new Object[newCapacity];
+
+        for (int i = 1; i <= size; i++) {
+            newArray[i] = array[i];
+        }
+
+        this.array = null;
+        this.array = newArray;
+    }
+
+    public void add(E value){
+
+        if(size + 1 == array.length){
+            resize(array.length * 2);
+        }
+
+        siftUp(size + 1, value);
+        size++;
+    }
+
+    private void siftUp(int idx, E target) {
+        if(comparator != null){
+            siftUpComparator(idx, target, comparator);
+        }else {
+            siftUpComparable(idx, target);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUpComparator(int idx, E target, Comparator<? super E> comp) {
+
+        while (idx > 1){
+            int parent = getParent(idx);
+            Object parentVal = array[parent];
+
+            if(comp.compare(target, (E) parentVal) >= 0){
+                break;
+            }
+
+            array[idx] = parentVal;
+            idx = parent;
+        }
+
+        array[idx] = target;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUpComparable(int idx, E target) {
+
+        Comparable<? super E> comp = (Comparable<? super E>) target;
+
+        while (idx > 1){
+            int parent = getParent(idx);
+            Object parentVal = array[parent];
+
+            if(comp.compareTo((E)parentVal) >= 0){
+                break;
+            }
+            array[idx] = parentVal;
+            idx = parent;
+        }
+        array[idx] = comp;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E remove(){
+        if(array[1] == null){
+            throw new NoSuchElementException();
+        }
+
+        E result = (E) array[1];
+        E target = (E) array[size];
+        array[size] = null;
+
+        siftDown(1, target);
+
+        return result;
+    }
+
+    private void siftDown(int idx, E target) {
+        if(comparator != null){
+            siftDownComparator(idx, target, comparator);
+        }else{
+            siftDownComparable(idx, target);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftDownComparator(int idx, E target, Comparator<? super E> comp) {
+
+        array[idx] = null;
+        size--;
+
+        int parent = idx;
+        int child;
+
+        while ((child = getLeftChild(parent)) <= size){
+
+            int right = getRightChild(parent);
+
+            Object childVal = array[child];
+
+            if(right <= size && comp.compare((E) childVal, (E) array[right]) > 0){
+                child = right;
+                childVal = array[child];
+            }
+
+            if(comp.compare(target, (E) childVal) <= 0){
+                break;
+            }
+
+            array[parent] = childVal;
+            parent = child;
+        }
+
+        array[parent] = target;
+
+        if(array.length > DEFAULT_CAPACITY && size <array.length / 4){
+            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftDownComparable(int idx, E target) {
+
+        Comparable<? super E> comp = (Comparable<? super E>) target;
+
+        array[idx] = null;
+        size--;
+
+        int parent = idx;
+        int child;
+
+        while ((child = getLeftChild(parent)) <= size){
+
+            int right = getRightChild(parent);
+
+            Object childVal = array[child];
+
+            if(right <= size && ((Comparable<? super E>)childVal).compareTo((E)array[right]) > 0){
+                child = right;
+                childVal = array[child];
+            }
+
+            if(comp.compareTo((E) childVal) <= 0){
+                break;
+            }
+            array[parent] = childVal;
+            parent = child;
+        }
+        array[parent] = comp;
+
+        if(array.length > DEFAULT_CAPACITY && size < array.length / 4){
+            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
+        }
     }
 }
