cc48cd88c2e06cac573038f58ec7613e222920e5
asp4303 <asp4303@gmail.com>
2022-01-24 16:18:49 +0900

    Set 인터페이스 생성

---
+++ src/Interface_from/Set.java
@@ -0,0 +1,18 @@
+package Interface_from;
+
+public interface Set<E> {
+
+    boolean add(E e);
+
+    boolean remove(Object o);
+
+    boolean contains(Object o);
+
+    boolean equals(Object o);
+
+    boolean isEmpty();
+
+    int size();
+
+    void clear();
+}
