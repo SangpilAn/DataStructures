98f82a19af0be0db49a65d5203b2e31d60fde1fb
asp4303 <asp4303@gmail.com>
2022-01-26 15:34:00 +0900

    LinkedHashSet 클래스 구현 완료

--- src/DataStructureClass/LinkedHashSet.java
+++ src/DataStructureClass/LinkedHashSet.java
@@ -88,31 +88,186 @@ public class LinkedHashSet<E> implements Set<E> {
         return null;
     }
 
+    @SuppressWarnings("unchecked")
     private void resize() {
+
+        int newCapacity = table.length * 2;
+
+        final Node<E>[] newTable = (Node<E>[]) new Node[newCapacity];
+
+        for (int i = 0; i < table.length; i++) {
+
+            Node<E> value = table[i];
+
+            if(value == null){
+                continue;
+            }
+
+            table[i] = null;
+
+            Node<E> nextNode;
+
+            do {
+
+                int idx = value.hash % newCapacity;
+
+                if(newTable[idx] != null){
+                    Node<E> tail = newTable[idx];
+
+                    while (tail.next != null){
+                        tail = tail.next;
+                    }
+
+                    nextNode = value.next;
+                    value.next = null;
+                    tail.next = value;
+                }else{
+                    nextNode = value.next;
+                    value.next = null;
+                    newTable[idx] = value;
+                }
+
+                value = nextNode;
+            }while (value != null);
+        }
+        table = null;
+        table = newTable;
+    }
+
+    private void unlinkNode(Node<E> o){
+        Node<E> prevNode = o.prevLink;
+        Node<E> nextNode = o.nextLink;
+
+        if(prevNode == null){
+            head = nextNode;
+        }else {
+            prevNode.nextLink = nextNode;
+            o.prevLink = null;
+        }
+
+        if(nextNode == null){
+            tail = prevNode;
+        }else {
+            nextNode.prevLink = prevNode;
+            o.nextLink = null;
+        }
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        return remove(hash(o), o) != null;
+    }
+
+    private Object remove(int hash, Object key) {
+
+        int idx = hash % table.length;
+
+        Node<E> node = table[idx];
+        Node<E> removedNode = null;
+        Node<E> prev = null;
+
+        if(node == null){
+            return null;
+        }
+
+        do {
+
+            if(node.hash == hash && (node.key == key || node.key.equals(key))){
+
+                removedNode = node;
+
+                if(prev == null){
+                    table[idx] = node.next;
+                }else {
+                    prev.next = node.next;
+                }
+
+                unlinkNode(node);
+                node = null;
+
+                size--;
+                break;
+            }
+            prev = node;
+            node = node.next;
+        }while (node != null);
+
+        return removedNode;
     }
 
     @Override
     public boolean contains(Object o) {
+        int idx = hash(o) % table.length;
+        Node<E> temp = table[idx];
+
+        while (temp != null){
+            if(o == temp.key || (o != null && temp.key.equals(o))){
+                return true;
+            }
+            temp = temp.next;
+        }
+
         return false;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void clear() {
+        if(table != null && size > 0){
+            for (int i = 0; i < table.length; i++) {
+                table[i] = null;
+            }
+            size = 0;
+        }
+        head = tail = null;
+    }
 
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean equals(Object o) {
+
+        if(o == this){
+            return true;
+        }
+
+        if(!(o instanceof LinkedHashSet)){
+            return false;
+        }
+
+        LinkedHashSet<E> oSet;
+
+        try{
+
+            oSet = (LinkedHashSet<E>) o;
+
+            if(oSet.size() != size){
+                return false;
+            }
+
+            for (int i = 0; i < oSet.table.length; i++) {
+                Node<E> oNode = oSet.table[i];
+
+                while (oNode != null){
+                    if(!contains(oNode)){
+                        return false;
+                    }
+                    oNode = oNode.next;
+                }
+            }
+
+        }catch (ClassCastException e){
+            return false;
+        }
+
+        return true;
     }
 }
